b000edc25a9e5c9ae5314631e9b73e77982a5cda
Nkumbe Aurelien <nkaurelien@gmail.com>
2018-12-01 03:19:59 +0100

    configuartion necessaire du app providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +17,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // set default database varchar length when migration is run
+        Schema::defaultStringLength(191);
+
+        // force secure url in production
+        if($this->app->environment('production') || $this->app->environment('staging')) {
+            URL::forceScheme('https');
+        }
+        // set default carbon package language to match the user language
+        Carbon::setLocale(Config::get('locale'));
     }
 
     /**
